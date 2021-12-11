7d505e3c6f5149be9b27023d24004ed0263daf60
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2021-12-11 14:40:35 -0800

    C is a sharp kitchen knife.

--- 01.c
+++ 01.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
 
-int number_of_increases()
-{
-    int reference, ahead;
+#define window_size 3
 
-    scanf("%i", &reference);
+int increases()
+{
+    int reference, next;
+
+    if (scanf("%i", &reference) != 1) return 0;
 
     int increases = 0;
-    while (scanf("%i", &ahead) != EOF) {
-        if (reference < ahead)
+    while (scanf("%i", &next) == 1) {
+        if (reference < next)
             ++increases;
-        reference = ahead;
+        reference = next;
     }
 
     return increases;
 }
 
+int sliding_increases()
+{
+    int reference, next, insert = 0;
+    int window[3];
+
+    if (scanf("%i %i %i", window, window + 1, window + 2) != 3) return 0;
+    reference = window[0] + window[1] + window[2];
+
+    int increases = 0;
+    while (scanf("%i", &next) == 1) {
+        int oldest = window[insert], newest = next;
+
+        window[insert] = newest;
+        insert = insert + 1 > window_size - 1 ? 0 : insert + 1;
+
+        int next_three = -oldest + reference + newest;
+        if (reference < next_three)
+            ++increases;
+        reference = next_three;
+    }
+    
+    return increases;
+}
+
 int main()
 {
-    int increases = number_of_increases();
-
-    printf("Number of increases: %i\n", increases);
+    // printf("Number of increases: %i\n", increases());
+    printf("Number of sliding increases: %i\n", sliding_increases());
 
     return 0;
 }
