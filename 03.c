f0b0552f55b0fd9db35a2d02d475dc23ffe53e2b
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2021-12-12 22:52:31 -0800

    I broke the program trying to refactor.

--- 03.c
+++ 03.c
@@ -8,42 +8,54 @@
 
 #include <stdio.h>
 
+enum common { more_zeros, more_ones, neither };
+
+int most_common(char report[][entry_length], int digit)
+{
+    int zeros = 0, ones = 0;
+    for (int entry = 0; entry < diagnostic_length; ++entry) {
+        char value = report[entry][digit];
+        if (value == '0') ++zeros;
+        else if (value == '1') ++ones;
+        else {
+            printf(
+                "abort: malformed value '%c' (entry %i, digit %i)\n",
+                value, entry, digit
+            );
+            return -1;
+        };
+        if (zeros < ones) return more_ones;
+        else if (zeros == ones) return neither;
+        else return more_zeros;
+    }
+}
+
 int power_consumption()
 {
     // Returns -1 on malformed input.
     // Careful of the narrowing of an unsigned product to int.
 
-    char diagnostic_report[diagnostic_length][entry_length + 1];
+    char report[diagnostic_length][entry_length];
 
     for (int entry = 0; entry < diagnostic_length; ++entry) {
-        if (scanf("%s", diagnostic_report + entry) != 1) return -1;
+        if (scanf("%s", report + entry) != 1) return -1;
     }
 
     unsigned gamma = 0, epsilon = 0;
 
     for (int digit = 0; digit < entry_length; ++digit) {
-        int zeros = 0, ones = 0;
-        for (int entry = 0; entry < diagnostic_length; ++entry) {
-            char value = diagnostic_report[entry][digit];
-            if (value == '0') ++zeros;
-            else if (value == '1') ++ones;
-            else {
-                printf(
-                    "abort: malformed value '%c' (entry %i, digit %i)\n",
-                    value, entry, digit
-                );
-                return -1;
-            };
-        }
-        if (zeros == ones) {
+        enum common which = most_common(report, digit);
+        printf("which: %i\n", which);
+        if (which == neither) {
             printf("abort: malformed digit %i\n", digit + 1);
             return -1;
         }
         gamma = gamma << 1; epsilon = epsilon << 1;
-        if (zeros < ones) gamma += 1;
+        if (which == more_ones) gamma += 1;
         else epsilon += 1;
     }
 
+    printf("gamma: %u, epsilon: %u\n", gamma, epsilon);
     return gamma * epsilon;
 }
 
