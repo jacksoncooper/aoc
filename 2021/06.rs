0011dc37cf3fd0395d33939231a87b03f583dda2
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2021-12-20 14:01:35 -0800

    Add Day 6.

---
+++ 2021/06.rs
@@ -0,0 +1,59 @@
+use std::io::{stdin, Read, BufRead, BufReader};
+use std::error::Error;
+use std::str::FromStr;
+
+const NOC: usize = 9;          // Number of clocks.
+const DTM: usize = 2;          // Days to maturity.
+type Clocks      = [u64; NOC]; // The fishies.
+
+fn main() -> Result<(), Box<dyn Error>>
+{
+    let days: u64 = 256;
+    let population: u64 = simulate(stdin(), days)?;
+    println!("After {} days, there are {} lanternfish.", days, population);
+    Ok(())
+}
+
+fn read_line<R : Read>(reader: R) -> std::io::Result<String>
+{
+    let mut buffer = String::new();
+    match BufReader::new(reader).read_line(&mut buffer) {
+        Ok(bytes_read) => {
+            println!("Read {} bytes of input.", bytes_read);
+            Ok(buffer)
+        },
+        Err(error) => Err(error)
+    }
+}
+
+fn make_clocks(line: String) -> Result<Clocks, <u64 as FromStr>::Err>
+{
+    let mut clocks: Clocks = [0; NOC];
+    for clock in line.trim().split(",") {
+        match clock.parse::<usize>() {
+            Ok(days_left) => clocks[days_left] += 1,
+            Err(error) => return Err(error)
+        }
+    }
+    Ok(clocks)
+}
+
+fn simulate<R: Read>(reader: R, days: u64) -> Result<u64, Box<dyn Error>>
+{
+    // [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    // ^                  |     ^
+    // time to spawn            time to mature
+
+    let mut clocks = make_clocks(read_line(reader)?)?;
+
+    for _ in 0..days {
+        let ready_to_spawn: u64 = clocks[0];
+        for days_left in 0..NOC - 1 {
+            clocks[days_left] = clocks[days_left + 1];
+        }
+        clocks[NOC - DTM - 1] += ready_to_spawn;
+        clocks[NOC - 1] = ready_to_spawn;
+    }
+
+    Ok(clocks.iter().sum())
+}
